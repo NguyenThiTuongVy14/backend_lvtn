3d71753e11e00916c51fe57c30ec27696f27938a
NguyenThiTuongVy <tuongvy140703@gmail.com>
2025-06-16 08:37:41 +0700

    log

--- src/main/java/com/example/test/service/CollectionPointService.java
+++ src/main/java/com/example/test/service/CollectionPointService.java
@@ -112,7 +112,7 @@ public class CollectionPointService {
             statusUpdate.put("updatedAt", jobRotation.getUpdatedAt());
 
             // Gửi update đến các app khác
-            System.out.println("Sending to WebSocket: " + statusUpdate);
+            System.out.println("Sending to WebSockettttt: " + statusUpdate);
             messagingTemplate.convertAndSend("/topic/job-status", statusUpdate);
 
         } catch (Exception e) {
